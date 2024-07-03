c4c4e71ab5d594ea8e0bf6feb1546b023cd6d5fc
MuryloSaladino <murylosaladino@gmail.com>
2024-07-03 11:15:49 -0300

    remove unused imports

--- src/main/java/com/bosch/realstatemanager/dto/user/UserCreationPayload.java
+++ src/main/java/com/bosch/realstatemanager/dto/user/UserCreationPayload.java
@@ -3,9 +3,7 @@ package com.bosch.realstatemanager.dto.user;
 import com.bosch.realstatemanager.entities.UserEntity;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import lombok.Builder;
 import lombok.Data;
 import lombok.Getter;
 

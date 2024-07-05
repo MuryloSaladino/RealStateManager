8d882e28705b2d0e3cb8d36e03cddcb099c1cb49
MuryloSaladino <murylosaladino@gmail.com>
2024-07-05 10:20:47 -0300

    feat: real state dto complete

--- src/main/java/com/realstatemanager/dto/realState/RealStateEntityResponse.java
+++ src/main/java/com/realstatemanager/dto/realState/RealStateEntityResponse.java
@@ -1,6 +1,6 @@
 package com.realstatemanager.dto.realState;
 
-
+import com.realstatemanager.dto.address.AddressEntityResponse;
 import com.realstatemanager.entities.RealStateEntity;
 
 public class RealStateEntityResponse {
@@ -8,10 +8,14 @@ public class RealStateEntityResponse {
     public Boolean sold;
     public Float price;
     public Integer squaredMeters;
+    public AddressEntityResponse address;
+    public String category;
 
     public RealStateEntityResponse(RealStateEntity realStateEntity) {
         this.sold = realStateEntity.getSold();
         this.price = realStateEntity.getPrice();
         this.squaredMeters = realStateEntity.getSquaredMeters();
+        this.address = new AddressEntityResponse(realStateEntity.getAddress());
+        this.category = realStateEntity.getCategory().getName();
     }
 }

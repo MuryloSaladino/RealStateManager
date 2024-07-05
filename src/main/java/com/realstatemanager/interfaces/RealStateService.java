8288ae61de9d7c26c519affbef174de5a77b5250
MuryloSaladino <murylosaladino@gmail.com>
2024-07-05 16:37:51 -0300

    feat: real state service interface

--- src/main/java/com/realstatemanager/interfaces/RealStateService.java
+++ src/main/java/com/realstatemanager/interfaces/RealStateService.java
@@ -1,5 +1,11 @@
 package com.realstatemanager.interfaces;
 
+import com.realstatemanager.dto.realState.RealStateCreationPayload;
+import com.realstatemanager.entities.RealStateEntity;
+
+import java.util.List;
+
 public interface RealStateService {
-    RealStateService create();
+    RealStateService create(RealStateCreationPayload payload);
+    List<RealStateEntity> readAll();
 }

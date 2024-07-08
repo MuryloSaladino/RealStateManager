69a50d56b5ac97e48b04f35af8f0f82ee25cbf4d
MuryloSaladino <murylosaladino@gmail.com>
2024-07-08 10:23:48 -0300

    fix: circular wiring

--- src/main/java/com/realstatemanager/interfaces/RealStateService.java
+++ src/main/java/com/realstatemanager/interfaces/RealStateService.java
@@ -1,11 +1,13 @@
 package com.realstatemanager.interfaces;
 
 import com.realstatemanager.dto.realState.RealStateCreationPayload;
+import com.realstatemanager.entities.CategoryEntity;
 import com.realstatemanager.entities.RealStateEntity;
 
 import java.util.List;
 
 public interface RealStateService {
-    RealStateService create(RealStateCreationPayload payload);
+    RealStateEntity create(RealStateCreationPayload payload);
     List<RealStateEntity> readAll();
+    List<RealStateEntity> readByCategory(Long id);
 }

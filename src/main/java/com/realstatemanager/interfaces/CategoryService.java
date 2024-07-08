b623f451c0bf6fa5a4c84868e0984c36cecd9ebf
MuryloSaladino <murylosaladino@gmail.com>
2024-07-08 10:22:04 -0300

    fix: circular wiring

--- src/main/java/com/realstatemanager/interfaces/CategoryService.java
+++ src/main/java/com/realstatemanager/interfaces/CategoryService.java
@@ -9,5 +9,4 @@ import java.util.List;
 public interface CategoryService {
     CategoryEntity create(CategoryCreationPayload payload);
     List<CategoryEntity> readAll();
-    List<RealStateEntity> readByCategory(Long id);
 }
